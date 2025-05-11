af16d1ddc3807393c16d21020484d8ca3c1ac395
Mamady KONTÉ <56128892+mamadykonte@users.noreply.github.com>
2025-05-11 16:32:06 +0200

    refactor : SearchBar replace native input with custom Input component
    Replace the native HTML input element with the custom Input component to ensure consistent styling and behavior across the application. Additionally, update the Button variant and hover color for better visual feedback.

--- components/common/SearchBar.tsx
+++ components/common/SearchBar.tsx
@@ -7,6 +7,7 @@ import { X } from "lucide-react";
 import { CARS_SEARCH_PARAMS } from "@/shared/constants";
 
 import { Button } from "../ui/button";
+import { Input } from "../ui/input";
 
 export default function SearchBar() {
   const router = useRouter();
@@ -17,7 +18,6 @@ export default function SearchBar() {
     searchParams.get(CARS_SEARCH_PARAMS.search) || ""
   );
 
-  // Quand les searchParams changent (ex: reset), on met à jour l'input
   useEffect(() => {
     setSearchTerm(searchParams.get(CARS_SEARCH_PARAMS.search) || "");
   }, [searchParams]);
@@ -52,22 +52,22 @@ export default function SearchBar() {
         <label htmlFor="search" className="sr-only">
           Rechercher une voiture
         </label>
-        <input
+        <Input
           id="search"
           type="text"
-          placeholder="Rechercher par marque ou modèle..."
+          placeholder="Rechercher par marque ou modèle"
+          className="placeholder:text-xs sm:placeholder:text-sm md:placeholder:text-base"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="w-full rounded-md border px-4 py-2 pr-10 text-sm focus:outline-none focus:ring focus:ring-primary/50"
           autoComplete="off"
         />
 
         {searchTerm && (
           <Button
             type="button"
-            variant="ghost"
+            variant="link"
             onClick={clearSearch}
-            className="absolute inset-y-0 right-3 flex items-center text-gray-400 hover:text-gray-600 focus:outline-none"
+            className="absolute inset-y-0 right-3 flex items-center text-gray-400 hover:text-red-600 focus:outline-none"
             aria-label="Effacer la recherche"
           >
             <X size={18} />
